99ad89cd0df4d8828d4bf4b1000197390596ce65
stim <141650876+stim87@users.noreply.github.com>
2023-09-26 22:37:12 +0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_12.1"
+export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_12.1"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="RMX2155"
+export OEM="realme"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
